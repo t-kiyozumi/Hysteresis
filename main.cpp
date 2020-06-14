9a146378ba541798c925b7689bb6e7bf74bbd405
t-kiyozumi <k.takaki@gmail.com>
2020-06-14 22:09:07 +0900

    A 5×5 square simulation

--- main.cpp
+++ main.cpp
@@ -6,30 +6,66 @@
 class dipole_state
 {
 public:
-  double arg = 3.14/2.11;
+  double arg = (2*M_PI)*(1.0/4.0);
   double length = 1.0;
   double potential = 1.0;
-  int enable = 1;
+  double enable = 1.0;
+};
+
+class field
+{
+  public:
+  double arg = (2.0*M_PI)*(1.0/4.0);
+  double strength = 0.5;
+  double strength_final = 0.5;
 };
 void display_cell(dipole_state[][7]);
 
 main()
 {
+//記録用ファイルの準備
+FILE *RecFile;
+RecFile = fopen("Rec.txt", "w");
+fprintf(RecFile, "number of trial,B,potential\n");
+
 
   int i, j;
   double k;
-  double torialNo = 1000000;
+  double torialNo =20000;
+  double allPotential = 0.0;
+  field magneticField;
   dipole_state magnetic[7][7];
   dipole_state rand_magnetic;
+
+  //無効セルの設定
+  //0行,0列,6行,6列を全て無効化
+  for (i = 0; i < 6; i++)
+  {
+    magnetic[i][0].enable = 0.0;
+  }
+  for (i = 0; i < 6; i++)
+  {
+    magnetic[i][6].enable = 0.0;
+  }
+  for (j = 0; j < 6; j++)
+  {
+    magnetic[0][j].enable = 0.0;
+  }
+  for (j = 0; j < 6; j++)
+  {
+    magnetic[6][j].enable = 0.0;
+  }
+
 
   //角度の初期化
   for (i = 1; i <= 5; i++)
   {
     for (j = 1; j <= 5; j++)
     {
-      // magnetic[i][j].arg = rand() % 360;                            //乱数で初期化，単位は[deg]
-      // magnetic[i][j].arg = ((2 * M_PI) / 360) * magnetic[i][j].arg; //[rad]に変換
-      magnetic[i][j].arg =((double)rand() / ((double)RAND_MAX + 1)) *(2*M_PI);
+    //ランダムな角度で初期化
+    //magnetic[i][j].arg =((double)rand() / ((double)RAND_MAX + 1)) *(2*M_PI);
+    //決まった角度で初期化
+    magnetic[i][j].arg = (2*M_PI)*(3.0/4.0);
     }
   }
 
@@ -40,10 +76,11 @@ main()
     for (j = 1; j <= 5; j++)
     {
       magnetic[i][j].potential =
-          -1.0 * (cos(magnetic[i + 1][j].arg - magnetic[i][j].arg)
-           + cos(magnetic[i - 1][j].arg - magnetic[i][j].arg)
-           + cos(magnetic[i][j + 1].arg - magnetic[i][j].arg) 
-           + cos(magnetic[i][j - 1].arg - magnetic[i][j].arg));
+          -1.0 * (cos(magnetic[i + 1][j].arg - magnetic[i][j].arg)*magnetic[i + 1][j].enable
+           + cos(magnetic[i - 1][j].arg - magnetic[i][j].arg)*magnetic[i -1][j].enable
+           + cos(magnetic[i][j + 1].arg - magnetic[i][j].arg)*magnetic[i][j+1].enable
+           + cos(magnetic[i][j - 1].arg - magnetic[i][j].arg))*magnetic[i][j-1].enable;
+      magnetic[i][j].potential = magnetic[i][j].potential-magneticField.strength*cos(magneticField.arg - magnetic[i][j].arg);
     }
   }
 
@@ -52,6 +89,8 @@ main()
 
 
   display_cell(magnetic);
+
+  //メインのループ
   for (k = 0; k < torialNo; k++)
   {
     printf("processing:%f %\n", (k / torialNo) * 100.0);
@@ -59,24 +98,29 @@ main()
     {
       for (j = 1; j <= 5; j++)
       {
-
-          rand_magnetic.arg = rand() % 360;
-          rand_magnetic.arg = ((2 * M_PI) / 360) * rand_magnetic.arg; //[rad]に変換
+        //ランダムに配置
+          rand_magnetic.arg = ((double)rand() / ((double)RAND_MAX + 1)) *(2*M_PI);
           rand_magnetic.potential =
-              -1.0 * (cos(magnetic[i + 1][j].arg - rand_magnetic.arg)
-                + cos(magnetic[i - 1][j].arg - rand_magnetic.arg)
-                + cos(magnetic[i][j + 1].arg - rand_magnetic.arg) 
-                + cos(magnetic[i][j - 1].arg - rand_magnetic.arg));
+              -1.0 * (cos(magnetic[i + 1][j].arg - rand_magnetic.arg)*magnetic[i + 1][j].enable
+                + cos(magnetic[i - 1][j].arg - rand_magnetic.arg)*magnetic[i - 1][j].enable
+                + cos(magnetic[i][j + 1].arg - rand_magnetic.arg)*magnetic[i][j+1].enable
+                + cos(magnetic[i][j - 1].arg - rand_magnetic.arg)*magnetic[i][j-1].enable);
+          rand_magnetic.potential = rand_magnetic.potential-magneticField.strength*cos(magneticField.arg - rand_magnetic.arg);
+
           if (rand_magnetic.potential < magnetic[i][j].potential)
           {
             magnetic[i][j].arg = rand_magnetic.arg;
             magnetic[i][j].potential = rand_magnetic.potential;
           }
+          allPotential=allPotential+magnetic[i][j].potential;
       }
     }
+    fprintf(RecFile, "%f,%f,%f\n",k,magneticField.strength,allPotential);
+    allPotential =0.0;
+    magneticField.strength=magneticField.strength-(magneticField.strength_final*2/torialNo);
   }
-
   display_cell(magnetic);
+  fclose(RecFile);
   return 0;
 }
 
@@ -89,9 +133,9 @@ void display_cell(dipole_state magnetic[][7])
   {
     for (j = 1; j <= 5; j++)
     {
-      //printf("|U=%3.2f:arg=%3.2f[deg]|", magnetic[i][j].potential, magnetic[i][j].arg*(360.0/(2*M_PI)));
+      printf("|U=%3.2f:arg=%3.2f[deg]|", magnetic[i][j].potential, magnetic[i][j].arg*(360.0/(2*M_PI)));
       //printf("|arg=%3.2f[deg]|",magnetic[i][j].arg*(360.0/(2*M_PI)));
-      printf("|U=%3.2f|", magnetic[i][j].potential);
+      //printf("|U=%3.2f|", magnetic[i][j].potential);
 
 
       //printf("|arg:%3.2f\t|", magnetic[i][j].potential, magnetic[i][j].arg);
